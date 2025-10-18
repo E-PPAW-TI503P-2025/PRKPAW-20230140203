9f790f089233667e8f210f26bf94e7049b65b313
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-18 10:47:24 +0700

    add Tugas3

--- 20230140203-node-server/server.js
+++ 20230140203-node-server/server.js
@@ -1,23 +1,28 @@
- 	const express = require('express');
- 	const cors = require('cors'); 
- 	const app = express();
- 	const PORT = 3001;
-	 const booksRouter=require("./routes/books")
- 	
- 	// Middleware
- 	app.use(cors()); 
-	app.use(express.json()); 
- 	app.use((req, res, next) => {
- 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
- 	  next();
- 	});
- 	
- 	app.get('/', (req, res) => {
- 	  res.send('Home Page for API');
- 	});
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
 
-  	app.use("/api/books", booksRouter);
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/report");
 
- 	app.listen(PORT, () => {
- 	  console.log(`Express server running at http://localhost:${PORT}/`);
- 	});
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Home Page for API");vs
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/report", reportRoutes);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
---
+++ Tugas/Tugas3.md
@@ -0,0 +1,11 @@
+
+# tugas 3
+
+## check-in sekali
+![alt text](ss/ss-tugas3/check-insekali.png)
+## check-in lebih dari sekali
+![alt text](ss/ss-tugas3/check-in-lebih-1.png)
+## check-out sekali
+![alt text](ss/ss-tugas3/checkout.png)
+## check-out lebih dari sekali
+![alt text](ss/ss-tugas3/checkout1.png)
