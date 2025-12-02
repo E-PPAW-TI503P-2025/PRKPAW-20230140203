8e4b5a383121706488509d975d5181b4264dfaf1
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-02 22:51:39 +0700

    add app.js dan file tugas md

--- 20230140203-react/src/App.js
+++ 20230140203-react/src/App.js
@@ -1,28 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import DashboardPage from './components/DashboardPage';
-import AttendancePage from './components/PresensiPage';
+import PresensiPage from './components/PresensiPage';
 import ReportPage from './components/ReportAdmin';
+import MainLayout from './components/MainLayout'
+
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarPaths = ['/dashboard', '/presensi', '/report'];
+  const shouldShowNavbar = !hideNavbarPaths.some(path => location.pathname.startsWith(path));
+
+  return (
+    <div className="min-h-screen font-sans">
+      {shouldShowNavbar && (
+        <nav className="absolute top-0 w-full p-6 flex justify-end space-x-8 text-[#850E35] font-bold z-50 pr-10">
+          <Link to="/login" className="hover:text-[#EE6983] transition-colors duration-200 hover:underline drop-shadow-sm">
+            Login
+          </Link>
+          <Link to="/register" className="hover:text-[#EE6983] transition-colors duration-200 hover:underline drop-shadow-sm">
+            Register
+          </Link>
+        </nav>
+      )}
+
+      <Routes>
+        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/presensi" element={<PresensiPage />} />
+        <Route element={<MainLayout />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/report" element={<ReportPage />} />
+        </Route>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav className="p-4 bg-gradient-to-r from-rose-100 to-pink-100 shadow-md">
-        </nav>
-        
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/presensi" element={<AttendancePage />} />
-          <Route path="/reports" element={<ReportPage />} />
-          <Route path="/" element={<LoginPage />} /> 
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
+
 export default App;
